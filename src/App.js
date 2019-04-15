c48f7472a21e9010ed58b359cc6d9d9f72d988a8
Ajay <ajay@cefalo.com>
2019-04-15 12:18:53 +0600

    Rename Method name

--- src/App.js
+++ src/App.js
@@ -18,20 +18,18 @@ class App extends Component {
     return (
       <div>
         <Search onSearchChange={term => this.onSearch(term)}/>
-        <ProductList products={this.state.products} onVoterIncrease={selectedProduct=> this.onVoterIncrease(selectedProduct)} />
+        <ProductList products={this.state.products} onIncreaseVotes={selectedProduct=> this.onIncreaseVotes(selectedProduct)} />
       </div>
     );
   }
 
   onSearch(searchText) {
-    console.log(`From App: ${searchText}`)
-    console.log(Products.posts)
     this.setState({
       products: Products.posts.filter(product => product.name.toLowerCase().indexOf(searchText.toLowerCase()) > -1).sort((first, last)=> last.votes_count - first.votes_count)
     })
   }
 
-  onVoterIncrease(selectedProduct) {
+  onIncreaseVotes(selectedProduct) {
     console.log(selectedProduct);
     this.setState({
       products: this.state.products.map(product => product.id === selectedProduct.id ? Object.assign({}, selectedProduct, {votes_count: selectedProduct.votes_count + 1}) : product).sort((first, last)=>  last.votes_count - first.votes_count)
